12d397bc058cbc150e7c1764ceefc6d9c53b9cb0
hanae <hanaeelyaagoubi45@gmail.com>
2023-11-28 08:15:52 -0800

    right

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(x)), end="")
+for char in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(char), end='')
