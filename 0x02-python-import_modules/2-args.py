db89c20b6791c483deaa79224f8ac8da5f2e761a
hanae <hanaeelyaagoubi45@gmail.com>
2023-11-30 09:37:46 -0800

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    arg_plural = "s" if num_args != 1 else ""
+    print("Number of argument{}: {}".format(arg_plural, num_args), end="")
+    if num_args == 0:
+        print(".")
+    else:
+        print(":")
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
